97de12df908a07e0a81c91a121e8ff3e0b25a559
Adi-Or <adi.rosenfeld15@gmail.com>
2022-08-28 19:52:20 +0300

    add design signup with bug
    הרשמה עובדת (המשתמש מתווסף) אבל דף הבית לא נטען לאחר ההרשמה וגם אימות הקלט לא מופיע למשתמש

--- main/views.py
+++ main/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from .models import Post
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import get_user_model, login, \
-                                logout, authenticate
-
+from django.contrib.auth import get_user_model, login, logout, authenticate
+import os
 # Create your views here.
 def home(request):
+    print("i am here")
     posts = Post.objects.all()
     context = {'posts': posts}
+    print("i am here2")
     return render(request, 'main/home.html', context)
 
 def post_list(request):
@@ -21,14 +22,19 @@ def signup(request):
                       {'form': UserCreationForm()})
     else:
         form = UserCreationForm(request.POST)
+        print("post")
         if form.is_valid():
+            print("is valid")
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             authenticate(username=username, password=raw_password)
             user = get_user_model().objects.get(username=username)
+            print("user created")
             login(request, user)
-            return render(request,'main/home.html')
+            print("logged in")
+            return home(request)
         else:
+            print("not valid")
             error = form.errors
             return render(request, 'registration/signup.html', {'form':UserCreationForm(), 'error': error})
